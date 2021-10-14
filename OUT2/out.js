18f28619f667ef71baf65e93be00424209584802
proyuli <proyuli@163.com>
2021-10-14 10:39:39 +0800

    OUT2

---
+++ OUT2/out.js
@@ -0,0 +1,27 @@
+
+var leftTime = 14400;
+
+
+function CountTime() {
+    //var date = new Date();
+    //var now = date.getTime();
+    //var endTime = now + 14400000;
+    leftTime--;
+    var d = Math.floor(leftTime / 60 / 60 / 24);
+    var h = Math.floor(leftTime / 60 / 60 % 24);
+    var m = Math.floor(leftTime / 60 % 60);
+    var s = Math.floor(leftTime % 60);
+    document.getElementById("day").innerHTML = " " + d + "天 ";
+    document.getElementById("hour").innerHTML = "0" + h + ":";
+    if (m > 9) {
+        document.getElementById("min").innerHTML = m + ":";
+    } else {
+        document.getElementById("min").innerHTML = "0" + m + ":";
+    }
+    if (s > 9) {
+        document.getElementById("sec").innerHTML = s;
+    } else {
+        document.getElementById("sec").innerHTML = "0" + s;
+    }
+    setTimeout(CountTime, 1000);
+}
